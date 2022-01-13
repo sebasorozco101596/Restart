12cdf3db0299da76f315ff0c5aa34d18c49d40a6
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-01-12 22:36:29 -0500

    Starting up with the new project named Restart

---
+++ Restart/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Juan Sebastian Orozco Buitrago on 1/12/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
